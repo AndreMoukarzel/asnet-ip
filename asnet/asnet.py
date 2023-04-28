0bb9f707b397b216b8ccf761bd708dfe72259960
Andre Ferrari Moukarzel <andremoukarzel@gmail.com>
2023-04-28 19:53:28 -0300

    Refactors ASNet into a class
    - ASNets are compatible with ippddl-parser package
    - Transform ASNets into a class

--- asnet/asnet.py
+++ asnet/asnet.py
@@ -6,88 +6,92 @@ from tensorflow.keras.layers import Input, Lambda, Dense, Concatenate
 from tensorflow.keras.models import Model
 
 
-# Blocksworld Domain
-actions = {'pick-up', 'pick-up-from-table', 'put-on-block', 'put-down', 'pick-tower', 'put-tower-on-block', 'put-tower-down'}
-prepositions = {'holding', 'emptyhand', 'on-table', 'on', 'clear'}
-relations = [
-    {'emptyhand', 'clear', 'on', 'holding', 'on-table'}, # pick-up
-    {'emptyhand', 'clear', 'holding', 'on-table'}, # pick-up-from-table
-    {'holding', 'clear', 'on', 'on-table'}, # put-on-block
-    {'holding', 'clear', 'on-table', 'emptyhand'}, # put-down
-    {'emptyhand', 'clear', 'on', 'holding'}, # pick-tower
-    {'holding', 'on', 'clear', 'emptyhand', 'on-table'}, # put-tower-on-block
-    {'holding', 'on', 'on-table', 'emptyhand'} # put-tower-down
-]
+
+class ASNet:
+
+    def __init__(self, action_relations: dict, predicate_relations: dict) -> None:
+        self.model = self._create_asnet(action_relations, predicate_relations)
 
 
-def values_to_index(uninedexed_dict: dict, indexing_list: list):
-    indexed_dict = {}
-    for key, values in uninedexed_dict.items():
-        indexed_values = [indexing_list.index(val) for val in values]
-        indexed_values.sort()
-        indexed_dict[key] = indexed_values
-    return indexed_dict
+    def _values_to_index(self, uninedexed_dict: dict, indexing_list: list):
+        """
+        """
+        indexed_dict = {}
+        for key, values in uninedexed_dict.items():
+            indexed_values = [indexing_list.index(val) for val in values]
+            indexed_values.sort()
+            indexed_dict[key] = indexed_values
+        return indexed_dict
 
 
-def get_actions_from_layer(layer, action_indexes: List[int]) -> Lambda:
-    """Returns an intermediary Lambda layer that receives all inputs from the
-    specified layer and outputs only the values of specified action_indexes.
+    def _get_actions_from_layer(self, layer, action_indexes: List[int]) -> Lambda:
+        """Returns an intermediary Lambda layer that receives all inputs from the
+        specified layer and outputs only the values of specified action_indexes.
 
-    Essentially serves as a mask to filter only outputs from desired actions.
-    """
-    return Lambda(lambda x: tf.gather(x, action_indexes, axis=1))(layer)
-
-
-def build_prepositions_layer(prev_layer, prepositions, act_indexed_relations, layer_num):
-    """Builds a preposition layer.
-    """
-    prepositions_layer = []
-    for prep in prepositions:
-        related_actions_indexes = act_indexed_relations[pred]
-        prep_neuron = Dense(1, name=f"{prep}{layer_num}")(
-            get_actions_from_layer(prev_layer, related_actions_indexes)
-        ) # Only connects the preposition neuron to related actions
-        prepositions_layer.append(prep_neuron)
-    prep_layer = Concatenate(name=f"Prep{layer_num}")(prepositions_layer)
-    return prep_layer
+        Essentially serves as a mask to filter only outputs from desired actions.
+        """
+        return Lambda(lambda x: tf.gather(x, action_indexes, axis=1))(layer)
 
 
-def build_actions_layer(prev_layer, actions, pred_indexed_relations, layer_num):
-    """Builds an action layer.
-    """
-    actions_layer = []
-    for act in actions:
-        related_prep_indexes = pred_indexed_relations[act]
-        act_neuron = Dense(1, name=f"{act}{layer_num}")(
-            get_actions_from_layer(prev_layer, related_prep_indexes)
-        ) # Only connects the preposition neuron to related actions
-        actions_layer.append(act_neuron)
-    act_layer = Concatenate(name=f"Acts{layer_num}")(actions_layer)
-    return act_layer
+    def _build_prepositions_layer(self, prev_layer, prepositions, act_indexed_relations, layer_num):
+        """Builds a preposition layer.
+        """
+        prepositions_layer = []
+        for prep in prepositions:
+            related_actions_indexes = act_indexed_relations[prep]
+            prep_neuron = Dense(1, name=f"{prep}{layer_num}")(
+                self._get_actions_from_layer(prev_layer, related_actions_indexes)
+            ) # Only connects the preposition neuron to related actions
+            prepositions_layer.append(prep_neuron)
+        prep_layer = Concatenate(name=f"Prep{layer_num}")(prepositions_layer)
+        return prep_layer
 
 
-def create_asnet(actions: set, prepositions: set, action_relations: dict, predicate_relations: dict):
-    actions = list(actions)
-    prepositions = list(prepositions)
-    act_indexed_relations = values_to_index(predicate_relations, actions)
-    pred_indexed_relations = values_to_index(action_relations, prepositions)
-    print(act_indexed_relations)
-
-    inputs = Input(shape=(len(actions),), name="A1")
-
-    prep1 = build_prepositions_layer(inputs, prepositions, act_indexed_relations, 1)
-
-    act1 = build_actions_layer(prep1, actions, pred_indexed_relations, 1)
-
-    prep2 = build_prepositions_layer(act1, prepositions, act_indexed_relations, 2)
-
-    act2 = build_actions_layer(prep2, actions, pred_indexed_relations, 2)
-
-    #out = Dense(len(prepositions))(prep1)
-
-    return keras.Model(inputs, act2)
-
-model = create_asnet(actions, prepositions, relations)
+    def _build_actions_layer(self, prev_layer, actions, pred_indexed_relations, layer_num):
+        """Builds an action layer.
+        """
+        actions_layer = []
+        for act in actions:
+            related_prep_indexes = pred_indexed_relations[act]
+            act_neuron = Dense(1, name=f"{act}{layer_num}")(
+                self._get_actions_from_layer(prev_layer, related_prep_indexes)
+            ) # Only connects the preposition neuron to related actions
+            actions_layer.append(act_neuron)
+        act_layer = Concatenate(name=f"Acts{layer_num}")(actions_layer)
+        return act_layer
 
 
-keras.utils.plot_model(model, "my_first_model_with_shape_info.png", show_shapes=True)
+    def _create_asnet(self, action_relations: dict, predicate_relations: dict):
+        actions = [act for act in action_relations.keys()]
+        prepositions = [pred for pred in predicate_relations.keys()]
+        act_indexed_relations = self._values_to_index(predicate_relations, actions)
+        pred_indexed_relations = self._values_to_index(action_relations, prepositions)
+
+        inputs = Input(shape=(len(actions),), name="A1")
+
+        prep1 = self._build_prepositions_layer(inputs, prepositions, act_indexed_relations, 1)
+
+        act1 = self._build_actions_layer(prep1, actions, pred_indexed_relations, 1)
+
+        prep2 = self._build_prepositions_layer(act1, prepositions, act_indexed_relations, 2)
+
+        act2 = self._build_actions_layer(prep2, actions, pred_indexed_relations, 2)
+
+        #out = Dense(len(prepositions))(prep1)
+
+        return Model(inputs, act2)
+
+
+if __name__ == "__main__":
+    from ippddl_parser.parser import Parser
+
+    domain = '../problems/deterministic_blocksworld/domain.pddl'
+    problem = '../problems/deterministic_blocksworld/pb1.pddl'
+    parser = Parser()
+    parser.scan_tokens(domain)
+    parser.scan_tokens(problem)
+    parser.parse_domain(domain)
+    parser.parse_problem(problem)
+    asnet = ASNet(parser.action_relations, parser.predicate_relations)
+
+    keras.utils.plot_model(asnet.model, "asnet.png", show_shapes=True)
--- main.py
+++ main.py
@@ -1,30 +1,7 @@
-from ppddl_parser import _ppddl_tokenize, get_actions, get_predicates, get_predicate_relations, get_action_relations
-from asnet.asnet import create_asnet
+from asnet.asnet import ASNet
 
 import keras
 
 
 if __name__ == "__main__":
-    # Blocksworld Domain
-    with open('problems/blocksworld/domain.pddl', 'r') as fp:
-        pddl_txt = fp.read()
-        tokens = _ppddl_tokenize(pddl_txt)
-
-    actions = get_actions(tokens)
-    predicates = get_predicates(tokens)
-    action_relations = {}
-    predicate_relations = {}
-
-    for act in actions:
-        action_relations[act] = get_action_relations(tokens, act, get_predicates(tokens))
-    for pred in predicates:
-        predicate_relations[pred] = get_predicate_relations(pred, action_relations)
-
-    print(actions)
-    print(predicates)
-    print(action_relations)
-    print(predicate_relations)
-
-    model = create_asnet(actions, predicates, action_relations, predicate_relations)
-
-    keras.utils.plot_model(model, "model_shape.png", show_shapes=True)
+    pass
